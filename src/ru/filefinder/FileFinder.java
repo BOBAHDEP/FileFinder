237931dd2d237c60587f84058a1520a0d48bc31a
BOBAHDEP <vo.ryzhikh@mail.ru>
2015-11-15 17:46:10 +0400

    minor changes

--- src/ru/filefinder/FileFinder.java
+++ src/ru/filefinder/FileFinder.java
@@ -1,19 +1,20 @@
 package ru.filefinder;
 
 import java.io.File;
-import java.rmi.server.ServerRef;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 /**
  * The FileFinder class provides file search in
  * specific directory {@link #fileFolder} using
- * regex as a name identifier in {@link #findFiles(String[] fileNameExpr)}
+ * regex as a name identifier in {@link #findFiles(String regex, String needDate, String needSize)}
  *
- * @author  Ryzhikh Vladimir
+ * @author Ryzhikh Vladimir
  */
 public class FileFinder {
-    /** Pathname string of a directory where search is being conducted*/
+    /**
+     * Pathname string of a directory where search is being conducted
+     */
     private String fileFolder;
 
     /**
@@ -27,20 +28,20 @@ public class FileFinder {
 
     /**
      * Writes an information about one file to console in one
-     * line. Depending on flags needData ans needSize can provide
+     * line. Depending on flags needDate ans needSize can provide
      * additional information about last date of modification of
      * file and it's size in bytes respectively.
      *
      * @param file          file of interest
-     * @param needData      flag of additional information about last date of modification.
+     * @param needDate      flag of additional information about last date of modification.
      *                      Can be either "-d" or null
      *                      if "-d" then information is printed. If null, it is not printed.
      * @param needSize      flag of additional information about file size in bytes.
      *                      Can be either "-s" or null
      *                      if "-s" then information is printed. If null, it is not printed.
      */
-    private void writeFileInformation(File file, String needData, String needSize) {
-        if (needData.equals("-d")) {
+    private void writeFileInformation(File file, String needDate, String needSize) {
+        if (needDate != null && needDate.equals("-d")) {
             Date lastModified = new Date(file.lastModified());
             SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
             System.out.print(formatter.format(lastModified) + " ");
@@ -57,12 +58,17 @@ public class FileFinder {
     }
 
     /**
-     * Searches for files in folderPath according to regexp, given as fileNameExp[0]
+     * Searches for files in folderPath according to regexp, given as regex
      * and prints information about them to console
      *
-     * @param folderPath    path of folder where fies are being searched
-     * @param fileNameExp   regex expression defines file name and date, size
-     *                      flags
+     * @param folderPath    path of folder where files are searched
+     * @param regex         regex expression by witch file name is chosen
+     * @param needDate      flag of additional information about last date of modification.
+     *                      Can be either "-d" or null
+     *                      if "-d" then information is printed. If null, it is not printed.
+     * @param needSize      flag of additional information about file size in bytes.
+     *                      Can be either "-s" or null
+     *                      if "-s" then information is printed. If null, it is not printed.
      */
     private void findFiles(String folderPath, String regex, String needDate, String needSize) {
 
@@ -85,15 +91,26 @@ public class FileFinder {
     }
 
     /**
-     * Searches for files in folderPath according to regexp, given as fileNameExp[0]
+     * Searches for files in folderPath according to regexp, given asregex]
      * and prints information about them to console. Starts in {@link #fileFolder}
      * folder and then acts recursively for folders
      *
-     * @param fileNameExpr  regex expression defines file name and date, size
-     *                      flags
+     * @param regex         regex expression by witch file name is chosen
+     * @param needDate      flag of additional information about last date of modification.
+     *                      Can be either "-d" or null
+     *                      if "-d" then information is printed. If null, it is not printed.
+     * @param needSize      flag of additional information about file size in bytes.
+     *                      Can be either "-s" or null
+     *                      if "-s" then information is printed. If null, it is not printed.
      */
     public void findFiles(String regex, String needDate, String needSize) {
-        System.out.println("Date of last change        Size(bytes)    isDirection?      Name                         Folder\n");
+        if (needDate != null && needSize != null) {
+            System.out.println("Date of last change|       Size(bytes)   |isDirection?|     Name                |        Folder\n");
+        } else if (needDate != null) {
+            System.out.println("Date of last change|isDirection?|     Name                |        Folder\n");
+        }else {
+            System.out.println("isDirection?|     Name                |        Folder\n");
+        }
         findFiles(fileFolder, regex, needDate, needSize);
     }
 
--- src/ru/filefinder/Main.java
+++ src/ru/filefinder/Main.java
@@ -6,6 +6,13 @@ package ru.filefinder;
 public class Main {
     public static void main(String[] args) {
 
+        if (args.length == 0) {
+            System.out.println("You should call this program with following arguments:\n" +
+                    "regex expression; (optionally) -s; (optionally) -d.\n" +
+                    "-s will print additional information about size, -d about date");
+            return;
+        }
+
         FileFinder fileFinder = new FileFinder(System.getProperty("user.dir"));
         fileFinder.findFiles(ConsoleArgsParse.getNormalisedArgs(args)[0], ConsoleArgsParse.getNormalisedArgs(args)[1], ConsoleArgsParse.getNormalisedArgs(args)[2]);
 
